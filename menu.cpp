c8794141d1dc38488e82d4e13f1ff73f0093140f
Wajiha Arif <hackrgirl787@gmail.com>
2025-08-22 12:35:40 +0500

    New commit

---
+++ menu.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std ; 
+
+int main()
+{ 
+    //  1- Print help 
+     cout << "1- Print help" << endl ;
+    
+    // 2- Print exchange stats
+    cout << "2- Print exchange stats" << endl ; 
+
+    // 3- Make an offer 
+    cout << "3- Make an offer" << endl ; 
+
+    // 4- Make a bid 
+    cout << "4- Make a bid" << endl ; 
+
+    // 5- Print wallet 
+    cout << "5- Print wallet" << endl ; 
+
+    //  6- continue 
+    cout << "6- Continue" << endl ;  
+
+     cout << "========================" << endl ; 
+
+     int UserOption ; 
+      
+     cout << "Type in 1 to 6 : " ; 
+     cin >> UserOption ;  
+     cout << "You choose option " << UserOption << " to proceed." << endl ; 
+
+     cout << "=========================" << endl ; 
+
+     if(UserOption == 1)
+     { 
+       
+     }
+
+     else if(UserOption == 2)
+     {
+         
+     }
+     
+     else if(UserOption == 3)
+     {
+         
+     }
+
+     else if(UserOption == 4)
+     {
+         
+     }
+
+     else if(UserOption == 5)
+     {
+         
+     }
+
+     else if(UserOption == 6)
+     {
+         
+     }
+
+     else
+     {
+         
+     }
+
+     return 0 ; 
+}
