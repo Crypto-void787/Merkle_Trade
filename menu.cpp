b9b49b141f0f08ff6c9a0e17b70125f64248a94a
Wajiha Arif <hackrgirl787@gmail.com>
2025-08-22 13:21:58 +0500

    New commit

--- menu.cpp
+++ menu.cpp
@@ -3,68 +3,80 @@ using namespace std ;
 
 int main()
 { 
-    //  1- Print help 
-     cout << "1- Print help" << endl ;
-    
-    // 2- Print exchange stats
-    cout << "2- Print exchange stats" << endl ; 
+    while(true)
+    { 
+        //  1- Print help 
+        cout << "1- Print help" << endl ;
+        
+        // 2- Print exchange stats
+        cout << "2- Print exchange stats" << endl ; 
 
-    // 3- Make an offer 
-    cout << "3- Make an offer" << endl ; 
+        // 3- Make an offer 
+        cout << "3- Make an offer" << endl ; 
 
-    // 4- Make a bid 
-    cout << "4- Make a bid" << endl ; 
+        // 4- Make a bid 
+        cout << "4- Make a bid" << endl ; 
 
-    // 5- Print wallet 
-    cout << "5- Print wallet" << endl ; 
+        // 5- Print wallet 
+        cout << "5- Print wallet" << endl ; 
 
-    //  6- continue 
-    cout << "6- Continue" << endl ;  
+        //  6- continue 
+        cout << "6- Continue" << endl ;  
 
-     cout << "========================" << endl ; 
+        // 7- exit
+        cout << "7- Exit" << endl ; 
 
-     int UserOption ; 
-      
-     cout << "Type in 1 to 6 : " ; 
-     cin >> UserOption ;  
-     cout << "You choose option " << UserOption << " to proceed." << endl ; 
+        cout << "__________________________________" << endl ; 
 
-     cout << "=========================" << endl ; 
+        int UserOption ; 
+        
+        cout << "Type in 1 to 7 : " ; 
+        cin >> UserOption ;  
+        cout << "You choose option " << UserOption << " to proceed." << endl ; 
 
-     if(UserOption == 1)
-     { 
-       
-     }
+        cout << "__________________________________" << endl ; 
 
-     else if(UserOption == 2)
-     {
-         
-     }
-     
-     else if(UserOption == 3)
-     {
-         
-     }
 
-     else if(UserOption == 4)
-     {
-         
-     }
+        if(UserOption == 1)
+            { 
+            cout << "Help- Your aim is to make money. Analyse the market and make bids and offer." << endl ; 
+            }
 
-     else if(UserOption == 5)
-     {
-         
-     }
+        else if(UserOption == 2)
+            {
+                cout << "Market looks terrific! " << endl ;
+            }
+        
+        else if(UserOption == 3)
+            {
+                cout << "Mark an offer - enter the amount : " << endl ; 
+            }
 
-     else if(UserOption == 6)
-     {
-         
-     }
+        else if(UserOption == 4)
+            {
+                cout << "Make a bid - enter the amount : " << endl ; 
+            }
 
-     else
-     {
-         
-     }
+        else if(UserOption == 5)
+            {
+                cout << "Your wallet is empty. " << endl ;
+            }
 
+        else if(UserOption == 6)
+            {
+                cout << "Going to next time fram. " ; 
+            }
+        else if (UserOption == 7)
+            {  
+               cout << "Exiting program... Bye!" << endl;
+               break;  // breaks the loop -> program ends
+            }
+        else
+            {
+                cout << "Invalid choice! Choose between 1-6  " << endl ; 
+                
+            }
+
+    }
      return 0 ; 
 }
