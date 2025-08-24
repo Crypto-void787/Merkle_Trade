61e8fa640e03e1c531d566166cd55616bc1dd0c8
Wajiha Arif <hackrgirl787@gmail.com>
2025-08-24 20:31:57 +0500

    New commit

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std ; 
 
-int main()
-{ 
-    while(true)
-    { 
-        //  1- Print help 
+// We are making function to make our code modular 
+//Print menu function  
+  void Printmenu()
+  {
+     //  1- Print help 
         cout << "1- Print help" << endl ;
         
         // 2- Print exchange stats
@@ -28,55 +28,141 @@ int main()
 
         cout << "__________________________________" << endl ; 
 
-        int UserOption ; 
-        
+   
+  }
+   
+
+  int GetUser()
+   {
         cout << "Type in 1 to 7 : " ; 
+
+        int UserOption ; 
         cin >> UserOption ;  
-        cout << "You choose option " << UserOption << " to proceed." << endl ; 
+        cout << "You choose option " << UserOption << " to proceed." << endl ;  
 
-        cout << "__________________________________" << endl ; 
+        cout << "\n###################################################" << endl ; 
+        cout << endl ;
+        
+        return UserOption ; 
+   }
 
+   //Print help function 
 
+    void PrintHelp()
+    { 
+          cout << "Help-"<< endl ; 
+            cout << "     ---->  Your aim is to make money. Analyse the market and make bids and offer." << endl ; 
+            cout << "     ----> Further more you need to select an option and then follow the instruction that comes next." << endl ; 
+
+    }
+     
+    //print market stats function 
+    void Marketstats()
+    {
+        cout << " Market's poppin' off! Prices look wild  " << endl ;
+    }
+
+    //Offer making function
+    void OfferMaking()
+    {
+        cout << " Drop an offer, boss! How much ya flexin' : " << endl ; 
+    }
+
+    //  Bid placing function 
+    void PlaceBid()
+    {
+         cout << "Time to place a bid! Make a bid buddy : " << endl ; 
+    }
+
+    //Wallet printing function 
+    void  PrintWallet()
+    {
+          cout << " Wallet lookin' empty... time to stack some coins fam. " << endl ;
+    }
+
+     // Continue printing function 
+     void PrintContinue()
+     { 
+         cout << "Fast-forwarding to the next timeframe... hang tight! " ; 
+     }
+
+     // Exit function 
+
+     void exit()
+     { 
+            cout << "Exiting program... Bye!" << endl;
+     }
+
+     // Invalid choice print function 
+     void InvalidChoice()
+     { 
+        cout << "Invalid choice! Choose between 1-6  " << endl ; 
+     }
+   // Process user option 
+    
+    void ProcessUserOption(int UserOption)
+    { 
+         while(true)
+         {
         if(UserOption == 1)
             { 
-            cout << "Help- Your aim is to make money. Analyse the market and make bids and offer." << endl ; 
+                 PrintHelp() ;
             }
 
         else if(UserOption == 2)
             {
-                cout << "Market looks terrific! " << endl ;
+               Marketstats() ;
             }
         
         else if(UserOption == 3)
             {
-                cout << "Mark an offer - enter the amount : " << endl ; 
+               OfferMaking() ; 
             }
 
         else if(UserOption == 4)
             {
-                cout << "Make a bid - enter the amount : " << endl ; 
+              PlaceBid() ; 
             }
 
         else if(UserOption == 5)
             {
-                cout << "Your wallet is empty. " << endl ;
+               PrintWallet(); 
             }
 
         else if(UserOption == 6)
             {
-                cout << "Going to next time fram. " ; 
+               PrintContinue(); 
             }
         else if (UserOption == 7)
             {  
-               cout << "Exiting program... Bye!" << endl;
+               exit() ;
                break;  // breaks the loop -> program ends
             }
         else
             {
-                cout << "Invalid choice! Choose between 1-6  " << endl ; 
+                InvalidChoice() ;
                 
             }
-
+               
+            cout << "\n###################################################" << endl ; 
     }
-     return 0 ; 
+            cout << "\n###################################################" << endl ; 
+ } 
+    
+
+
+
+int main()
+{ 
+    while(true)
+    {  
+
+        Printmenu(); // Calling our function  to print the menu 
+        
+        int UserOption = GetUser() ; // function to take user's input 
+        ProcessUserOption(UserOption); //funtion to proceed with user option 
+
+        
+             return 0 ; 
+    }
 }
