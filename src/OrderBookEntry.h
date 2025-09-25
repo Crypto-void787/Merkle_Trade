18c4d9ef733040a304a7802f218bd2ac2ced880b
Wajiha Arif <hackrgirl787@gmail.com>
2025-09-25 17:05:00 +0500

    Add OrderBookEntry header defining the structure

--- src/OrderBookEntry.h
+++ src/OrderBookEntry.h
@@ -3,26 +3,43 @@
 #include<string> 
 
 
-enum class OrderBookType{bid , ask, unknown } ;
+enum class OrderBookType{bid , ask, unknown, asksale, bidsale } ;
  
    class OrderBookEntry
    {
        public:
 
        OrderBookEntry(
-          const std::string& _Timestamp,
-          const std::string& _Product,
-          OrderBookType _OrderType,
-          double _Price,
-          double _Amount  );
+                        std::string _Timestamp,
+                        std::string  _Product,
+                        OrderBookType _OrderType,
+                        double _Price,
+                        double _Amount,
+                        std::string _username = "dataset" ) ; 
 
    static OrderBookType StringToOrderBookType(const std::string& s) ;  
+   
+   static bool CompareByTimestamp(OrderBookEntry& e1, OrderBookEntry& e2) 
+   {
+       return e1.Timestamp < e2.Timestamp ; 
+   }
 
-      std::string Timestamp;
-      std::string Product;
-      OrderBookType OrderType;
-      double Price;
-      double Amount;
+   static bool CompareByPriceAsc(OrderBookEntry& e1, OrderBookEntry& e2) 
+   {
+       return e1.Price < e2.Price ; 
+   }
+
+   static bool CompareByPriceDecs(OrderBookEntry& e1, OrderBookEntry& e2) 
+   {
+       return e1.Price > e2.Price ; 
+   }
+
+      std::string Timestamp ;
+      std::string Product ;
+      OrderBookType OrderType ;
+      double Price ;
+      double Amount ;
+      std::string username ; 
 
     
    };
