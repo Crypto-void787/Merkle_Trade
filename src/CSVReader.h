35a0fc49cf556ed268b42b68b0016498d214b281
Wajiha Arif <hackrgirl787@gmail.com>
2025-09-25 17:04:59 +0500

    Add CSVReader header with method declarations

--- src/CSVReader.h
+++ src/CSVReader.h
@@ -15,10 +15,16 @@
       CSVReader() ; 
       
       static std::vector<OrderBookEntry> readCSV(std::string csvFile) ; 
+      static std::vector<std::string> tokenise(std::string csvLine , char separator) ; 
+     
+      static OrderBookEntry StringsToOBE( std::string Timestamp,
+                                          std::string Product, 
+                                          OrderBookType orderType,
+                                          std::string Amount, 
+                                          std::string Price ) ; 
       
       private:
         
-        static std::vector<std::string> tokenise(std::string csvLine , char separator) ; 
         static OrderBookEntry StringsToOBE(std::vector<std::string> strings) ; 
 
     } ; 
